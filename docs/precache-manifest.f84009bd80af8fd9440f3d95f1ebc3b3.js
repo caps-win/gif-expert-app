2df75cc5239104cc5b9120501b7ee1e114c8e783
Juan Sebastian <jsantahernandez@gmail.com>
2020-08-11 11:50:57 -0500

    add docs

---
+++ docs/precache-manifest.f84009bd80af8fd9440f3d95f1ebc3b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6938c37bdd2073b79974f2c896f076d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c7249983aebeb5dee2f",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "73f9fd330fb7ce52e599",
+    "url": "/static/css/main.af72dac7.chunk.css"
+  },
+  {
+    "revision": "9c7249983aebeb5dee2f",
+    "url": "/static/js/2.6b770aca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6b770aca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f9fd330fb7ce52e599",
+    "url": "/static/js/main.4b3ed08d.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
